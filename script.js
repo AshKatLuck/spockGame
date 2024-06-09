f704276e3b75206ea18f6d5af9f34ea953395b03
Asha Luckins <ashaluckinsanoop@gmail.com>
2024-06-08 20:11:56 -0400

    functionality complete

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+import { startConfetti, stopConfetti, removeConfetti } from "./confetti.js"
 const playerEl=document.getElementById("player");
 const playerChoiceEl=document.getElementById("playerChoice");
 const computerEl=document.getElementById("computer");
@@ -16,7 +17,11 @@ const computerScissors=document.getElementById("computerScissors");
 const computerLizard=document.getElementById("computerLizard");
 const computerSpock=document.getElementById("computerSpock");
 
+const playerScoreEl=document.getElementById("playerScore");
+const computerScoreEl=document.getElementById("computerScore");
+
 const allGameIcons=document.querySelectorAll(".far");
+const resetBtn=document.querySelector(".reset-icon");
 
 const choices = {
   rock: { name: 'Rock', defeats: ['scissors', 'lizard'] },
@@ -27,6 +32,8 @@ const choices = {
 };
 
 let computerChoice='';
+let computerScore=0;
+let playerScore=0;
 
 //function to clear selected class from icons
 function clearSelected(){
@@ -43,20 +50,67 @@ function getRandomInt(min, max) {
 
 function computerRandomChoice(){
   const computerChoiceNumber=getRandomInt(0,4);
-  const computerChoice=choices[Object.keys(choices)[computerChoiceNumber]].name;
-  console.log(computerChoiceNumber,computerChoice.toLowerCase());
-  displayComputerChoice(computerChoice.toLowerCase());
+  computerChoice=choices[Object.keys(choices)[computerChoiceNumber]].name;
+  // console.log(computerChoiceNumber,computerChoice.toLowerCase());
+  computerChoice=computerChoice.toLowerCase()
+  displayComputerChoice();
+}
+
+function scoreCheck(){
+  if((playerScore==5)||(computerScore==5)){
+    clearScore();
+    if(playerScore==5){
+      console.log("you won");
+    }else{
+      console.log("computer won");
+    }
+  }
 }
 
 //function to check Result
-function checkresult(){
+function checkresult(playerChoice){
   clearSelected();
   computerRandomChoice();
+  // console.log(playerChoice, computerChoice, choices[computerChoice].defeats);
+  if(choices[computerChoice].defeats.includes(playerChoice)){
+    computerScore++;
+    computerScoreEl.textContent=computerScore;
+    stopConfetti();
+    removeConfetti();
+    resultTextEl.textContent="Computer Won!"
+    // console.log('computer won');  
+  }else if(computerChoice==playerChoice){
+    resultTextEl.textContent="Draw!"
+    stopConfetti();
+    removeConfetti();
+    // console.log("draw");
+  }
+  else{
+    playerScore++; 
+    playerScoreEl.textContent=playerScore;
+    startConfetti();
+    resultTextEl.textContent="You Won!";
+    
+    // console.log("player won");      
+  }
+  scoreCheck();  
+}
+
+
+
+//function to clear score
+function clearScore(){
+  playerScore=0;
+  computerScore=0;
+  playerScoreEl.textContent=playerScore;
+  computerScoreEl.textContent=computerScore;
+  stopConfetti();
+  removeConfetti();
 }
 
 //function to set the computer Style
 
-function displayComputerChoice(computerChoice){
+function displayComputerChoice(){
   switch(computerChoice){
     case 'rock':
       computerRock.classList.add('selected');
@@ -87,7 +141,7 @@ function displayComputerChoice(computerChoice){
 
 //function to style th selected item
 function select(playerChoice){
-  checkresult();
+  checkresult(playerChoice);
   switch(playerChoice){
     case 'rock':
       playerRock.classList.add('selected');
@@ -114,4 +168,10 @@ function select(playerChoice){
     
   }
 }
+window.select=select;
 
+//event listener
+resetBtn.addEventListener('click', clearScore);
+
+
+//Confetti.js
